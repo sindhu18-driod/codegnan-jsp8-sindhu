b46905a9373b75a1933ef67ac19316c1a5bfb499
sindhu18-driod <c.sindhupriya2003@gmail.com>
2024-07-23 12:05:23 +0530

    Add files via upload

---
+++ CodeGnan/day34.js
@@ -0,0 +1,9 @@
+//input methods
+var x=prompt("Enter your name");
+var z=confirm("Are are 18 years old");
+if(z==true){
+    console.log("welcome "+x +" You can vote");
+}
+else{
+    console.log("Please try  after 18 Years");
+} 
